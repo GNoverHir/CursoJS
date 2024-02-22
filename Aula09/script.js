a0f6cdeebb282d819edb5bf91be6707a211e5675
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-02-22 18:10:48 -0300

    First Commit Aula09

---
+++ Aula09/script.js
@@ -0,0 +1,6 @@
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName("p")[1]
+
+window.document.write("Esta Escrito Assim: " + p1.innerText)
+p1.style.color = "blue"
+corpo.style.backgroundColor = "black"
