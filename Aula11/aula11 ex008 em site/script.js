804d53e467a16606335b2fc4f76878744c48d81b
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-02-23 20:41:45 -0300

    3. commit aula11

--- Aula11/aula11 ex008 em site/script.js
+++ Aula11/aula11 ex008 em site/script.js
@@ -1,3 +1,13 @@
 function verificar () {
-    
+    var txt = document.querySelector("input#txt")
+    var pais = String(txt.value).toLowerCase()
+    var resposta = document.querySelector("div#Resposta")
+    resposta.innerHTML = `<p>Voce é <strong>${pais}</strong>!</p>`
+
+
+    if (pais == "br"  ) {
+        resposta.innerHTML += `<p>Você é <strong>Brasileiro(a)!</strong></p>`
+    } else {
+        resposta.innerHTML += `<p>Você é <strong>estrangeiro!</strong></p>`
+    }
 }
